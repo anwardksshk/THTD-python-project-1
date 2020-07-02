5d49b3819dd3b68e0bacbcbbf531c16ab15c19e0
Anwar Rizalman <anwar140812@gmail.com>
2020-07-02 21:44:47 +0800

    Complete 'meet-expectation' grade
    Basic functions for number guessing game.

--- guessing_game.py
+++ guessing_game.py
@@ -1,37 +1,25 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 import random
 
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+  print("Welcome to the Number Guessing Game")
+  random_number = int(random.randint(1,9))
+  attempt = 0
+  print("\nThe random number is " + str(random_number)) #temporary
+  while True:
+    guess_number = input("Pick a number between 1 to 10: ")
+    guess_number = int(guess_number)
+    print("Guess number is = {}.\n".format(guess_number))
+    attempt += 1
 
+    if guess_number == random_number:
+      break
+    elif guess_number < random_number:
+      print("Your guess number is LOWER than the answer!")
+    elif guess_number > random_number:
+      print("Your guess number is HIGHER than the answer!")
+  return(attempt)
 
+attempt = start_game()
+print("CONGRATULATIONS, you got it! It took you {} tries".format(attempt))
 
-# Kick off the program by calling the start_game function.
-start_game()
