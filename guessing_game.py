7f52630388d171689394e98bdaee47b8af3e0493
Anwar Rizalman <anwar140812@gmail.com>
2020-07-03 16:51:10 +0800

    Complete 'exceed expectation' grade'

--- guessing_game.py
+++ guessing_game.py
@@ -1,25 +1,53 @@
 import random
+high_score = None
+attempt = None
 
-
-def start_game():
-  print("Welcome to the Number Guessing Game")
-  random_number = int(random.randint(1,9))
+def start_game(high_score):
+  random_number = int(random.randint(1,10))
   attempt = 0
-  print("\nThe random number is " + str(random_number)) #temporary
-  while True:
-    guess_number = input("Pick a number between 1 to 10: ")
-    guess_number = int(guess_number)
-    print("Guess number is = {}.\n".format(guess_number))
-    attempt += 1
+  print("The current high score is {}".format(high_score))
+  #temporary
+  print("\nThe random number is " + str(random_number)) 
 
-    if guess_number == random_number:
-      break
-    elif guess_number < random_number:
-      print("Your guess number is LOWER than the answer!")
-    elif guess_number > random_number:
-      print("Your guess number is HIGHER than the answer!")
+  while True:
+    guess_number = ""
+    try:
+      guess_number = int(input("\nPick a number between 1 to 10: "))
+      if guess_number > 10 or guess_number < 1:
+        raise ValueError("Your guess is outside the guessing range! Try again!")
+    except ValueError as err:
+      print("That is an invalid value! Please try again.")
+      print("({})".format(err))
+    except NameError:
+      print("Name Error: Please try again.")
+    else:
+      print("Guess number is = {}.\n".format(guess_number))
+      attempt += 1
+      if guess_number == random_number:
+        break
+      elif guess_number < random_number:
+        print("Your guess number is LOWER than the answer!")
+      elif guess_number > random_number:
+         print("Your guess number is HIGHER than the answer!")
+
   return(attempt)
 
-attempt = start_game()
-print("CONGRATULATIONS, you got it! It took you {} tries".format(attempt))
+print("Welcome to the Number Guessing Game")
+attempt = start_game(high_score)
+print("CONGRATULATIONS, you got it! It took you {} tries.".format(attempt))
+if high_score == None or high_score > attempt:
+  high_score = attempt
+  print("You set a record! The new high score to beat is {}!".format(high_score))
 
+
+
+play_again = input("\nWould you like to play again? (Y/N) -> ")
+while play_again.lower() != 'n':
+    attempt = start_game(high_score  )
+    print("CONGRATULATIONS, you got it! It took you {} tries\n".format(attempt))
+    if high_score == None or high_score > attempt:
+      high_score = attempt
+      print("You set a record! The new high score to beat is {}!".format(high_score))
+    play_again = input("\nWould you like to play again? (Y/N) -> ")
+
+print("===END GAME===")
